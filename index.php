8c7bac952cd3c158c4641704974c471126241fa9
Emanuel Kronas <em.kronas@tnnk.local>
2026-03-12 08:59:29 +0100

    aktualisiert

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
-<<<<<<< HEAD
 header("Location: /barbershop/public/");
-=======
 require __DIR__ . '/app/config/config.php';
 header("Location: " . BASE_URL . "/");
->>>>>>> 0b8fdd1 (Initial upload from Windows path)
 exit;
